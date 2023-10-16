5f0811b3656ca0b532ecad8d52592623bf09d46a
Camilo Zapata <camilo.zapata@nebulae.com.co>
2023-10-16 16:51:19 -0500

    initial commit

--- kmesg.go
+++ kmesg.go
@@ -6,11 +6,57 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 func TailKmesg(ctx context.Context) (chan string, error) {
+
+	f, err := os.Open("/dev/kmsg")
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := f.Seek(0, io.SeekEnd); err != nil {
+		return nil, err
+	}
+
+	watcher := time.NewTicker(1 * time.Second)
+
+	reader := bufio.NewReader(f)
+
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer watcher.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("close kmesg read")
+				return
+			case <-watcher.C:
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					fmt.Printf("error reading line: %s", err)
+					return
+				}
+				fmt.Println("line form kmesg ", line) // process the line
+				select {
+				case <-ctx.Done():
+				case ch <- line:
+				default:
+				}
+			}
+		}
+	}()
+
+	return ch, nil
+}
+
+func TailKmesg_DEPRECATED(ctx context.Context) (chan string, error) {
 
 	f, err := os.Open("/dev/kmsg")
 	if err != nil {

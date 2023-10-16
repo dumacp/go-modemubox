90686b1055035584e30722584a420cd36fc10a8c
Camilo Zapata <camilo.zapata@nebulae.com.co>
2023-10-16 11:11:39 -0500

    initial commit

--- cmd/modemubox/iptest.go
+++ cmd/modemubox/iptest.go
@@ -18,7 +18,7 @@ func ping(iptest string, count int, wait, waittime time.Duration) error {
 		waittimeseconds = 1
 	}
 
-	pingcmd := fmt.Sprintf("ping -c %d, -i %d -W %d", count, waitseconds, waittimeseconds)
+	pingcmd := fmt.Sprintf("ping -c %d, -i %d -W %d %s", count, waitseconds, waittimeseconds, iptest)
 	out, err := exec.Command("/bin/sh", "-c", pingcmd).Output()
 	if err != nil {
 		return fmt.Errorf("ping cmd error, %q, %w", out, err)
